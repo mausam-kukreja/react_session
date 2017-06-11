49a5dbf196c6aa0ac5637eaaa99cf15e21fd1bce
Saaheel Majethia <saaheelm.m@peopleinteractive.in>
2017-06-12 00:05:42 +0530

    Some files added

--- tic_tac_toe_redux/src/App.js
+++ tic_tac_toe_redux/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Board from './Board';
 
 class App extends Component {
   render() {
@@ -8,11 +9,15 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Tic Tac Toe</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>          
+        </div>
+        
       </div>
     );
   }
